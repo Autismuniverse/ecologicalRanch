368f59240a798cd4970205fe4ac6a5719b1b1501
Autisticuniverse <1322399234@qq.com>
2020-07-12 14:40:30 +0800

    2020 7月8日做的折扣，实现根据订购时间折扣

--- src/main/java/com/ecologicalRanch/project/controller/LoginHandler.java
+++ src/main/java/com/ecologicalRanch/project/controller/LoginHandler.java
@@ -1,59 +1,61 @@
 package com.ecologicalRanch.project.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.ecologicalRanch.project.result.CommonResult;
 import com.ecologicalRanch.utils2.HttpUtil;
 import io.swagger.annotations.Api;
-import org.springframework.web.bind.annotation.PostMapping;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.io.IOException;
 @Api(tags = "微信登录")
 @RestController
+@Log4j2
 public class LoginHandler {
 
     private String appID = "wx4bd8b4a9b1e3c26e";
     private String appSecret = "bfffc7550da959acf43dc02013585b0d";
 
-    @PostMapping("/we/login")
+    @GetMapping("/we/login")
 
-    public CommonResult userLogin(@RequestBody String code) throws IOException {
-        String result = "";
-        String str = code;
+    public CommonResult userLogin(@RequestBody JSONObject code) {
 
-        //判断第一个字符是否为“{”
-        if (str.startsWith("{")) {
-            str = str.substring(1);
-        }
+        System.out.println(code);
 
-        //判断最后一个字符是否为“}”
-        if (str.endsWith("}")) {
-            str = str.substring(0,str.length() - 1);
-        }
+//        //判断第一个字符是否为“{”
+//        if (code.startsWith("{")) {
+//            code = code.substring(1);
+//        }
+//
+//        //判断最后一个字符是否为“}”
+//        if (code.endsWith("}")) {
+//            code = code.substring(0, code.length() - 1);
+//        }
 
-        String str1 = str.replaceAll("\"", "");
-        String str2 = str1.replaceAll("code:", "");
-        try{//请求微信服务器，用code换取openid。HttpUtil是工具类，后面会给出实现，Configure类是小程序配置信息，后面会给出代码
-            result = HttpUtil.doGet(
+//        String str1 = code.replaceAll("\"", "");
+//        String str2 = str1.replaceAll("code:", "");
+
+        try {//请求微信服务器，用code换取openid。HttpUtil是工具类，后面会给出实现，Configure类是小程序配置信息，后面会给出代码
+            System.out.println(
                     "https://api.weixin.qq.com/sns/jscode2session?appid="
                             + appID + "&secret="
                             + appSecret + "&js_code="
-                            + str2
-                            + "&grant_type=authorization_code", null
-                            );
+                            + code.getString("code")
+                            + "&grant_type=authorization_code"
+            );
+
+            return CommonResult.success(HttpUtil.doGet(
+                "https://api.weixin.qq.com/sns/jscode2session?appid="
+                        + appID + "&secret="
+                        + appSecret + "&js_code="
+                        + code.getString("code")
+                        + "&grant_type=authorization_code", null
+            ));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            return CommonResult.failed();
         }
-
-        catch (Exception e) {
-            e.printStackTrace();
-        }
-
-        System.out.println("https://api.weixin.qq.com/sns/jscode2session?appid="
-                + appID + "&secret="
-                + appSecret + "&js_code="
-                + str2
-                + "&grant_type=authorization_code");
-
-        return CommonResult.success(result);
     }
 
 }
